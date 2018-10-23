1126fa425cdf6f6f466f5e1c0516d78fef8cff96
ftatp <ftatp5901@gmail.com>
2018-10-23 22:16:24 +0900

    ajax post

--- blog_visualizer_server/analysis/views.py
+++ blog_visualizer_server/analysis/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+	if request.method == "POST":
+		print(request.POST)
+
 	return HttpResponse("Hello, world. You're at the polls index.")
