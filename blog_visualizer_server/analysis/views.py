e846cb4bb116d9cabf2b88f801a67cf1a1457a01
ftatp <ftatp5901@gmail.com>
2018-10-29 19:57:27 +0900

    New code

---
+++ blog_visualizer_server/analysis/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def index(request):
+	if request.method == "POST":
+		print(request.POST)
+#return JsonResponse({"kk": "aaaa"})
+
+	return HttpResponse("Hello, world. You're at the index.")
