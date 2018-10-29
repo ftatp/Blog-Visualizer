adbf563baa52037fd73dd7d002cba3511d35f435
ftatp <ftatp5901@gmail.com>
2018-10-29 20:05:24 +0900

    Communication done

--- blog_visualizer_server/analysis/views.py
+++ blog_visualizer_server/analysis/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
 	if request.method == "POST":
 		print(request.POST)
+		
+		return HttpResponse("hello!! " + request.POST['search_list'])
 #return JsonResponse({"kk": "aaaa"})
 
 	return HttpResponse("Hello, world. You're at the index.")
