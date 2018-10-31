0d90d2d8deac6da59bb28b837ee27c58875520bc
YongGeol <yonggeol@YongGeolui-MacBook-Pro.local>
2018-10-31 21:50:05 +0900

    model update & Clustering update

---
+++ Data_preprocessing/Data_preprocessing.py
@@ -0,0 +1,12 @@
+class Datapreprocessing():
+    def __init__(self,html_code):
+        self.html = html_code
+
+    # Text_preprocessing
+        # 형태소 분석
+        # Sentiment
+        # 1인칭 2인칭 등등
+
+    # TFIDF - model
+
+    #
