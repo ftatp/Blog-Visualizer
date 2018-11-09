15d05646b80f575f24d7556537256b1160dc0e58
ftatp <ftatp5901@gmail.com>
2018-11-09 13:59:10 +0900

    preprocessing loaded

--- blog_visualizer_server/analysis/views.py
+++ blog_visualizer_server/analysis/views.py
@@ -5,15 +5,27 @@ from django.views.decorators.csrf import csrf_exempt
 import os
 import pandas as pd
 
+from analysis import Main_data_preprocessing_20181106
+
 @csrf_exempt
 def index(request):
 	if request.method == "POST":
 		print(request.POST)
 		
 		data_csv = pd.read_csv("static/cluster_mean.csv")
-		data_dict = data_csv.T.to_dict()
-		print(data_dict)
+		User_id = 'newpark314'
+		Post_id = '221387605004'
+		Category = '맛집'
 
+		post_data = Main_data_preprocessing_20181106.get_naver_post_all_data(User_id, Post_id, Category)
+		cluster_dict = data_csv.T.to_dict()
+		
+		data_dict = {
+			'post': post_data,
+			'clusters': cluster_dict
+		}
+		print("hdfgx")
+		print(data_dict)
 
 		#return HttpResponse("hello!! " + request.POST['search_list'])
 		return JsonResponse(data_dict)
