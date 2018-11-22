409e0fcc6fe7b383e3a8445ef035c5d016eafda1
ftatp <ftatp5901@gmail.com>
2018-11-22 10:54:53 +0900

    Uploaded code to match with ubuntu

---
+++ NaverExtension/script.js
@@ -0,0 +1,67 @@
+var loginPayload = {
+	"d": "dfsjrskrtdsktd"
+
+};
+
+var callback = function (response) {
+	console.log(response);
+};
+
+var handle_error = function (obj, error_text_status){
+	console.log(error_text_status + " " + obj);
+};
+
+/*
+$.ajax({
+	url: 'https://127.0.0.1:8000/analysis',
+	type: 'POST',
+	success: callback,
+	data: JSON.stringify(loginPayload),
+	contentType: 'application/json',
+	error: handle_error
+});
+*/
+
+function Success(data, textStatus, jqXHR){
+	//$('#loading').attr('style', 'visibility:hidden');
+	$('#resultDiv').html(data);
+	d3_data = data;
+	console.log("HIIIIIIII");
+	console.log(d3_data);
+}
+
+$(function(){
+	$('#clickme').on("click", function(){
+		//var tr = $(this);
+		//var td = tr.children();
+		var tmp = "HI"; //+ td.eq(1).text().split(" ")[0]
+		//$(this).siblings().css("background-color", "white");
+		//$(this).css("background-color", "#D3FFFE");
+		loginPayload.username = document.getElementById('name').value;
+		loginPayload.password = document.getElementById('details').value;
+		console.log(loginPayload);
+
+
+		$.ajax({
+			type: "POST",
+			url: "http://127.0.0.1:8000/analysis/",
+			data: {
+				'search_list': tmp + loginPayload.username,
+				'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
+			},
+			
+		success: Success,
+		dataType: 'html'
+		});
+//
+//		var xhr = new XMLHttpRequest();
+//		xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
+//		xhr.open("GET", chrome.extension.getURL('http://127.0.0.1:8000'), true);
+//		xhr.send();
+	});
+
+//	$("#prescription_text_1").text("총 {{ prescriptions_cnt }}개 데이터 검색됨");
+});
+
+
+
